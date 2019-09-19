28665932b197bfccce08f3c675a0670872d6a254
Raine Makelainen <raine.makelainen@jolla.com>
2019-09-19 18:44:13 +0300

    Update copyright & license headers

--- declarative/declarativetransfermodel.h
+++ declarative/declarativetransfermodel.h
@@ -1,28 +1,26 @@
-/****************************************************************************************
-**
-** Copyright (C) 2014-2016 Jolla Ltd.
-** Contact: Marko Mattila <marko.mattila@jolla.com>
-** All rights reserved.
-**
-** This file is part of Nemo Transfer Engine package.
-**
-** You may use this file under the terms of the GNU Lesser General
-** Public License version 2.1 as published by the Free Software Foundation
-** and appearing in the file license.lgpl included in the packaging
-** of this file.
-**
-** This library is free software; you can redistribute it and/or
-** modify it under the terms of the GNU Lesser General Public
-** License version 2.1 as published by the Free Software Foundation
-** and appearing in the file license.lgpl included in the packaging
-** of this file.
-**
-** This library is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-** Lesser General Public License for more details.
-**
-****************************************************************************************/
+/*
+ * Copyright (c) 2014 - 2019 Jolla Ltd.
+ *
+ * All rights reserved.
+ *
+ * This file is part of Sailfish Transfer Engine package.
+ *
+ * You may use this file under the terms of the GNU Lesser General
+ * Public License version 2.1 as published by the Free Software Foundation
+ * and appearing in the file license.lgpl included in the packaging
+ * of this file.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * and appearing in the file license.lgpl included in the packaging
+ * of this file.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
 
 #ifndef DECLARATIVETRANSFERMODEL_HH
 #define DECLARATIVETRANSFERMODEL_HH
